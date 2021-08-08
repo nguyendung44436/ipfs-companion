5041dbfb440269a1e022732d892283169c01825f
nguyendung44436 <nguyendung44436@gmail.com>
2021-08-08 22:45:03 +0700

    upload

--- add-on/src/lib/constants.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-/* eslint-env browser, webextensions */
-
-exports.welcomePage = '/dist/landing-pages/welcome/index.html'
-exports.optionsPage = '/dist/options/options.html'
-exports.tickMs = 250 // no CPU spike, but still responsive enough
--- test/helpers/mock-i18n.js
+++
@@ -1,8 +0,0 @@
-module.exports = () => ({
-  getMessage (key, substititions) {
-    if (!substititions) return key
-    substititions = Array.isArray(substititions) ? substititions : [substititions]
-    if (!substititions.length) return key
-    return `${key}[${substititions}]`
-  }
-})
